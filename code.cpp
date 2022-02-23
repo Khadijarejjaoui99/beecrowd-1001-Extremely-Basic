18a37bdb3f54ff690c420afe70756553da23903b
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 19:20:11 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,13 @@
+// Solution of 1001 - Extremely Basic in beecrowd
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int A, B, X;
+    cin >> A >> B;
+    X = A + B;
+    cout << "X = " << X << endl;
+    system("pause");
+    return 0;
+}
